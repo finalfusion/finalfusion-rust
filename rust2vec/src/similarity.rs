140d7b2159af3134738fe64f9722aaf687914457
Daniël de Kok <me@danieldk.eu>
2019-02-18 09:30:46 +0100

    Remove some mutability warnings

--- rust2vec/src/similarity.rs
+++ rust2vec/src/similarity.rs
@@ -360,7 +360,7 @@ mod tests {
     fn test_similarity() {
         let f = File::open("testdata/similarity.bin").unwrap();
         let mut reader = BufReader::new(f);
-        let mut embeddings = Embeddings::read_word2vec_binary(&mut reader, true).unwrap();
+        let embeddings = Embeddings::read_word2vec_binary(&mut reader, true).unwrap();
 
         let result = embeddings.similarity("Berlin", 40);
         assert!(result.is_some());
@@ -387,7 +387,7 @@ mod tests {
     fn test_similarity_limit() {
         let f = File::open("testdata/similarity.bin").unwrap();
         let mut reader = BufReader::new(f);
-        let mut embeddings = Embeddings::read_word2vec_binary(&mut reader, true).unwrap();
+        let embeddings = Embeddings::read_word2vec_binary(&mut reader, true).unwrap();
 
         let result = embeddings.similarity("Stuttgart", 10);
         assert!(result.is_some());
@@ -405,7 +405,7 @@ mod tests {
     fn test_analogy() {
         let f = File::open("testdata/analogy.bin").unwrap();
         let mut reader = BufReader::new(f);
-        let mut embeddings = Embeddings::read_word2vec_binary(&mut reader, true).unwrap();
+        let embeddings = Embeddings::read_word2vec_binary(&mut reader, true).unwrap();
 
         let result = embeddings.analogy("Paris", "Frankreich", "Berlin", 40);
         assert!(result.is_some());
