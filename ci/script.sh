2d285c971db0112ef7f15fa833383e97fb98bcc4
Daniël de Kok <me@danieldk.eu>
2019-05-13 09:35:17 +0200

    Check formatting on CI builds

---
+++ ci/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+cargo build
+cargo test
+
+# On Rust 1.31.0, we only care about passing tests.
+if [ ! rustc --version | grep "^rustc 1.31.0" ]; then
+  cargo fmt --all -- --check
+  cargo clippy -- -D warnings
+fi
+
+
