7ce10040912164dcce3400576bc6bda7bd1ae9a4
Daniël de Kok <me@danieldk.eu>
2019-07-17 19:22:57 +0200

    Fix CI script, so that rustfmt and clippy run again

--- ci/script.sh
+++ ci/script.sh
@@ -6,7 +6,7 @@ cargo build
 cargo test
 
 # On Rust 1.31.0, we only care about passing tests.
-if [ ! rustc --version | grep "^rustc 1.31.0" ]; then
+if rustc --version | grep -v "^rustc 1.31.0"; then
   cargo fmt --all -- --check
   cargo clippy -- -D warnings
 fi
