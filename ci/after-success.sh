cee177598181a5856615b8e46e2d625e7fab4f0b
Daniël de Kok <me@danieldk.eu>
2019-10-01 09:16:16 +0200

    Fix branch benchmark

--- ci/after-success.sh
+++ ci/after-success.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ] && [ "$TRAVIS_RUST_VERSION" == "stable" ]; then
     REMOTE_URL="$(git config --get remote.origin.url)";
     cd ${TRAVIS_BUILD_DIR}/.. && \
@@ -7,8 +9,8 @@ if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ] && [ "$TRAVIS
     cd  "${TRAVIS_REPO_SLUG}-bench" && \
     # Bench master
     git checkout master && \
-    cargo bench subword && \
+    cargo bench --bench subword && \
     # Bench pull request
     git checkout ${TRAVIS_COMMIT} && \
-    cargo bench subword ;
+    cargo bench --bench subword
 fi
