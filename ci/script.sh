70400115ca15fcdc4d2bcec9ddde680f0c262df1
NianhengWu <nianheng.wu@student.uni-tuebingen.de>
2019-10-02 13:57:46 +0200

    Move bench into 'ci/script'.

--- ci/script.sh
+++ ci/script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ex
 
@@ -11,4 +11,16 @@ if rustc --version | grep -v "^rustc 1.32.0"; then
   cargo clippy -- -D warnings
 fi
 
+if [ "${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" != "master" ] && [ "$TRAVIS_RUST_VERSION" == "stable" ]; then
+    REMOTE_URL="$(git config --get remote.origin.url)";
+    cd ${TRAVIS_BUILD_DIR}/.. && \
+    git clone ${REMOTE_URL} "${TRAVIS_REPO_SLUG}-bench" && \
+    cd  "${TRAVIS_REPO_SLUG}-bench" && \
+    # Bench master
+    git checkout master && \
+    cargo bench --bench subword && \
+    # Bench pull request
+    git checkout ${TRAVIS_COMMIT} && \
+    cargo bench --bench subword
+fi
 
