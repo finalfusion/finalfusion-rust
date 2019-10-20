c717ba4b2fe9cbb6a9f910e0a0534aa65d6ba0c8
Daniël de Kok <me@danieldk.eu>
2019-10-20 10:45:39 +0200

    Cross-compile for mips64-unknown-linux-gnuabi64
    mips64-unknown-linux-gnuabi64 is a big-endian platform.  Travis-CI
    configuration is from the byteorder project.

--- ci/script.sh
+++ ci/script.sh
@@ -2,13 +2,23 @@
 
 set -ex
 
-cargo build
-cargo test
+if [ ! -z "$CROSS_TARGET" ]; then
+  rustup target add "$CROSS_TARGET"
+  cargo install cross --force
+  export CARGO_CMD="cross"
+  export TARGET_PARAM="--target $CROSS_TARGET"
+else
+  export CARGO_CMD="cargo"
+  export TARGET_PARAM=""
+fi
+
+"$CARGO_CMD" build --verbose $TARGET_PARAM
+"$CARGO_CMD" test --verbose $TARGET_PARAM
 
 # Only run fmt and clippy against stable Rust 
-if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
-  cargo fmt --all -- --check
-  cargo clippy -- -D warnings
+if [ "$TRAVIS_RUST_VERSION" = "stable" ] && [ -z "$CROSS_TARGET" ]; then
+  "$CARGO_CMD" fmt --all -- --check
+  "$CARGO_CMD" clippy -- -D warnings
 fi
 
 
